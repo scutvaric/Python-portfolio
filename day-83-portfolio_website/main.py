0e6e410e335f2ac81d9451c6ee876998886c9a7b
scutvaric <112493123+scutvaric@users.noreply.github.com>
2025-07-28 12:06:43 +0200

    Task manager added

--- day-83-portfolio_website/main.py
+++ day-83-portfolio_website/main.py
@@ -8,7 +8,7 @@ from sqlalchemy.orm import relationship, DeclarativeBase, Mapped, mapped_column
 from sqlalchemy import Integer, String, Text
 from functools import wraps
 from werkzeug.security import generate_password_hash, check_password_hash
-from forms import CreatePostForm, RegisterForm, LoginForm, CommentForm
+from forms import CreatePostForm, RegisterForm, LoginForm
 import os
 from dotenv import load_dotenv
 load_dotenv("variables.env")
